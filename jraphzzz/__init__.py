0676f63923b4f0fc596caf8f9f8447421637e3c9
Syed Zayyan Masud <masudzayyan@gmail.com>
2025-10-03 10:50:23 +0100

    package renaming headaches

--- jraphzzz/__init__.py
+++ jraphzzz/__init__.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Jraph."""
+"""Jraphzzz."""
 
 
 from jraphzzz.data.graph import GraphsTuple
